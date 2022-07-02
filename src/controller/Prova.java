3ac23c19e1a8cea3de33ad60e994c74d3741c184
alessiobuccarella <alexbucc99@gmail.com>
2022-07-02 16:09:45 +0200

    ciao

--- src/controller/Prova.java
+++ src/controller/Prova.java
@@ -1,26 +1,9 @@
 package controller;
 
-
-import java.util.ArrayList;
-
-import javax.swing.JButton;
-
 import view.Menu;
 
-
-
 public class Prova {
-	ArrayList<JButton> manoGrafica=new ArrayList<JButton>();
-
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		new Menu();
-	
-
-
-		//new Frame();
 	}
-
-
 }
