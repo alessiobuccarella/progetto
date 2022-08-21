a573977732ba96bcb521bf15a3fe42579167c324
alessiobuccarella <alexbucc99@gmail.com>
2022-08-21 16:15:08 +0200

    prova

--- src/controller/Gioca.java
+++ src/controller/Gioca.java
@@ -1,21 +1,10 @@
 package controller;
 
-import java.awt.Graphics;
-import java.awt.Image;
-
-import javax.swing.JFrame;
-
-import view.Gioco;
 import view.Menu;
-import view.Piatto;
 import javax.swing.JPanel;
 
 public class Gioca extends JPanel{
-
-	
 	public static void main(String[] args) {
 		new Menu();
-		//new Gioco();
-
 	}
 }
