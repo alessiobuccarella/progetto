05d8b93fe813f1875ddd00d67c09028dec5db558
Francesco Buccarella <francesco.buccarella-renexter@rcibanque.com>
2022-08-31 11:26:42 +0200

    add db script

---
+++ db/init.sql
@@ -0,0 +1,18 @@
+CREATE USER 'app-user'@'localhost' IDENTIFIED BY 'password' ;
+CREATE DATABASE IF NOT EXISTS `jUno` ;
+GRANT ALL ON `jUno`.* TO 'app-user'@'localhost' ;
+FLUSH PRIVILEGES ;
+
+USE `jUno`;
+
+DROP TABLE IF EXISTS `Profilo`;
+CREATE TABLE `Profilo` (
+	`idProfilo` INT AUTO_INCREMENT NOT NULL,
+	`nickname` VARCHAR(45) NOT NULL,
+	`avatar` VARCHAR(45) NOT NULL,
+	`livello` INT DEFAULT 0,
+	`partite_giocate` INT DEFAULT 0,
+	`partite_vinte` INT DEFAULT 0,
+	`partite_perse` INT DEFAULT 0,
+	PRIMARY KEY (`idProfilo`,`nickname`)
+);
