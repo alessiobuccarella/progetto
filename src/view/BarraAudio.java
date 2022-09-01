ac6144f6bf4b278d02fd244dd9265d83047a85c1
Francesco Buccarella <francesco.buccarella-renexter@rcibanque.com>
2022-09-01 18:08:24 +0200

    costruttore barra audio

--- src/view/BarraAudio.java
+++ src/view/BarraAudio.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class BarraAudio extends JPanel {
-    BarraAudio() {
+    public BarraAudio() {
         setBackground(Color.black);
         JButton audioButton = new JButton("Audio");
         setLayout(new FlowLayout(FlowLayout.RIGHT));
