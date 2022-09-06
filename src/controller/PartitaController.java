5d9789501bee07e82bc3cdeebb96f3862f0c89c1
alessiobuccarella <alexbucc99@gmail.com>
2022-09-06 23:38:43 +0200

    fix

--- src/controller/PartitaController.java
+++ src/controller/PartitaController.java
@@ -1,55 +1,38 @@
 package controller;
 
-import model.Senso;
 import view2.PartitaPanel;
 
 public class PartitaController {
-	
-	
-	
+
 	 public PartitaController(PartitaPanel partitaPanel) {
-		 partitaPanel.funzionePasso(e ->{
+		 partitaPanel.funzionePasso(e -> {
 			 PartitaPanel.passo();
-	        System.out.println("ciao");
-	      
-	       	 });
+			 System.out.println("ciao");
+		 });
 
 		 partitaPanel.funzioneRosso(e ->{
 			 PartitaPanel.passo();
 			 PartitaPanel.cartaScarto.setC(0);
-			 
-			
-		        
 		 });
+
 		 partitaPanel.funzioneGiallo(e ->{
 			 PartitaPanel.passo();
 			 PartitaPanel.cartaScarto.setC(1);
-			 
-		        
 		 });
+
 		 partitaPanel.funzioneVerde(e ->{
 			 PartitaPanel.passo();
 			 PartitaPanel.cartaScarto.setC(3);
-	
-		        
 		 });
+
 		 partitaPanel.funzioneBlu(e ->{
 			 PartitaPanel.passo();
 			 PartitaPanel.cartaScarto.setC(2);
-		
-		        
 		 });
+
 		 partitaPanel.funzioneUno(e ->{
-			if (PartitaPanel.getDeviGridareUno()==true) PartitaPanel.setGridatoUno();
-		
-		        
+			if (PartitaPanel.getDeviGridareUno()==true)
+				PartitaPanel.setGridatoUno();
 		 });
-		 
-	 
-	 
-	 
-	 
-	       // avanti(gbc10, turno, manoOvest, manoNord, manoEst, piatto, postazioneOvest, postazioneNord, postazioneEst, mazzo, postazioneNord, manoEst,postazionePiatto);
-	    
-}
+	 }
 }
