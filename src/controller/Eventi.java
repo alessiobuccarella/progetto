105391c580950dba193c81dfd41f403b336f3cee
alessiobuccarella <alexbucc99@gmail.com>
2022-09-09 20:57:00 +0200

    updateDatabase e non updateDB2

--- src/controller/Eventi.java
+++ src/controller/Eventi.java
@@ -885,14 +885,14 @@ public class Eventi {
             musicObjectBot.playButtonMusic("./src/audio/defeat_audio.wav");
             JOptionPane.showMessageDialog(null, "" + frase);
             Database db2 = Database.getInstance();
-            db2.updateBD2(giocatore, false);
+            db2.updateDatabase(giocatore, false);
             Database.getInstance().close();
             System.exit(0);
         } else {
             musicObjectBot.playButtonMusic("./src/audio/victory_audio.wav");
             JOptionPane.showMessageDialog(null, "" + frase);
             Database db2 = Database.getInstance();
-            db2.updateBD2(giocatore, true);
+            db2.updateDatabase(giocatore, true);
             Database.getInstance().close();
             System.exit(0);
         }
