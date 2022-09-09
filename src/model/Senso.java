2084879e436e0be4a9cb52ade8c34bc340a37eeb
alessiobuccarella <alexbucc99@gmail.com>
2022-09-09 22:25:21 +0200

    Update Senso.java

--- src/model/Senso.java
+++ src/model/Senso.java
@@ -3,11 +3,14 @@ package model;
 /**
  * enum per la determinazione del senso di gioco
  */
-
 public enum Senso {
 
     /**
      * enum senso orario e antiorario della partita
      */
-    ORARIO, ANTIORARIO;
+    ORARIO,
+    /**
+     * enum senso orario e antiorario della partita
+     */
+    ANTIORARIO;
 }
