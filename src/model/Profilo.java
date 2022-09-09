b1d50c737a3ecedc1116d29bb9af9277a6de83b0
fabiower <fabiomassimonarducci@gmail.com>
2022-09-09 19:14:04 +0200

    javadoc

--- src/model/Profilo.java
+++ src/model/Profilo.java
@@ -44,6 +44,10 @@ public class Profilo {
 		 */
 		private int partitePerse;
 
+		/**
+		 * metodo che costruisce il profilo
+		 * @return il profilo
+		 */
 		public Profilo build() {
 			Profilo profilo = new Profilo();
 			profilo.nickname = this.nickname;
@@ -55,57 +59,111 @@ public class Profilo {
 			return profilo;
 		}
 
+		/**
+		 * setter del nickname del profilo
+		 * @param nickname scelto dal giocatore
+		 * @return il profilo aggiornato
+		 */
 		public ProfiloBuilder setNickname(String nickname) {
 			this.nickname = nickname;
 			return this;
 		}
 
+		/**
+		 * setter dell'avatar del giocato
+		 * @param avatarImg avatar scelto dal giocatore
+		 * @return il profilo aggiornato
+		 */
 		public ProfiloBuilder setAvatarImg(String avatarImg) {
 			this.avatarImg = avatarImg;
 			return this;
 		}
 
+		/**
+		 * setter del livello del giocatore
+		 * @param livello 
+		 * @return il profilo aggiornato
+		 */
 		public ProfiloBuilder setLivello(int livello) {
 			this.livello = livello;
 			return this;
 		}
 
+		/**
+		 * setter del numero di partite giocater
+		 * @param partiteGiocate
+		 * @return il profilo aggiornato
+		 */
 		public ProfiloBuilder setPartiteGiocate(int partiteGiocate) {
 			this.partiteGiocate = partiteGiocate;
 			return this;
 		}
 
+		/**
+		 * setter delle partite vinte
+		 * @param partiteVinte
+		 * @return il profilo aggiornato
+		 */
 		public ProfiloBuilder setPartiteVinte(int partiteVinte) {
 			this.partiteVinte = partiteVinte;
 			return this;
 		}
 
+		/**
+		 * setter delle partite perse
+		 * @param partitePerse
+		 * @return il profilo aggiornato
+		 */
 		public ProfiloBuilder setPartitePerse(int partitePerse) {
 			this.partitePerse = partitePerse;
 			return this;
 		}
 	}
 
+	/**
+	 * getter nickname
+	 * @return nickname
+	 */
 	public String getNickname() {
 		return nickname;
 	}
 
+	/**
+	 * getter dell'avatar
+	 * @return avatar del giocatore
+	 */
 	public String getAvatarImg() {
 		return avatarImg;
 	}
 
+	/**
+	 * getter del livello del giocatore
+	 * @return livello del giocatore
+	 */
 	public int getLivello() {
 		return livello;
 	}
 
+	/**
+	 * getter delle partite giocate
+	 * @return il numero delle partite giocate
+	 */
 	public int getPartiteGiocate() {
 		return partiteGiocate;
 	}
 
+	/**
+	 * getter delle partite vinte
+	 * @return il numero di partite vinte
+	 */
 	public int getPartiteVinte() {
 		return partiteVinte;
 	}
 
+	/**
+	 * getter delle partite perse
+	 * @return il numero di partite perse
+	 */
 	public int getPartitePerse() {
 		return partitePerse;
 	}
--- src/view2/DisegnaCarta.java
+++ src/view2/DisegnaCarta.java
@@ -4,8 +4,17 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import model.Carta;
 
+/**
+ * classe che restituisce la rappresentazione grafica della carta
+ */
 public class DisegnaCarta {
 
+    /**
+     * metodo che disegna la carta su un JButton costruendo il percorso dell'immagine 
+     * tramite i getter della cart
+     * @param carta da disegnare
+     * @return JButton che rappresenta graficamente la carta
+     */
     public static JButton disegnaCarta(Carta carta) {
         String filename = "./src/immagini/" + carta.getV() + carta.getC() + ".png";
         ImageIcon img = new ImageIcon(filename);
